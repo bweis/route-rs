2e6001105b2b09c418369fa3149bfaf6c09576fb
Collin Valley <collin.valley@gmail.com>
2019-07-16 23:09:54 -0700

    Split Element:  Test long stream
    This test checks that the element works even with a stream that is
    much longer than queue length, so we know that the consumer must
    sleep many times, and be awoken by the appropirate provider.

--- src/lib.rs
+++ src/lib.rs
@@ -368,4 +368,36 @@ mod tests {
         assert_eq!(elem0_port1_output, vec![1, 1337, 3, 5, 7, 9, 11]);
     }
 
+    #[test]
+    fn one_split_element_immediate_yield() {
+        let default_channel_size = 10;
+        let number_branches = 2;
+        let packet_generator = immediate_stream(0..=2000);
+
+        let elem0 = SplitEvenOddElement { id: 0 };
+
+        let mut elem0_link = SplitElementLink::new(Box::new(packet_generator), elem0, default_channel_size, number_branches);
+        let elem0_drain = elem0_link.consumer;
+
+        // Ordering is important since we are popping, too tired to do this cleaner :).   
+        let (s1, elem0_port1_collector_output) = crossbeam::crossbeam_channel::unbounded();
+        let elem0_port1_collector = ExhaustiveCollector::new(0, Box::new(elem0_link.providers.pop().unwrap()), s1);
+
+        let (s0, elem0_port0_collector_output) = crossbeam::crossbeam_channel::unbounded();
+        let elem0_port0_collector = ExhaustiveCollector::new(0, Box::new(elem0_link.providers.pop().unwrap()), s0);
+
+        tokio::run(lazy (|| {
+            tokio::spawn(elem0_drain);
+            tokio::spawn(elem0_port0_collector);
+            tokio::spawn(elem0_port1_collector);
+            Ok(())
+        }));
+
+        let elem0_port0_output: Vec<_> = elem0_port0_collector_output.iter().collect();
+        assert_eq!(elem0_port0_output.len(), 1001);
+
+        let elem0_port1_output: Vec<_> = elem0_port1_collector_output.iter().collect();
+        assert_eq!(elem0_port1_output.len(), 1000);
+    }
+
 }
